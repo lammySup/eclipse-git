7d92f4746b28ecac0a124bb3e3b0cd8783421df2
lammySup <laxmi28prakash@gmail.com>
2019-02-24 15:30:56 +0530

    Gotcha Sample GitHUb

---
+++ GitHub/src/com/sample/github/HelloGitHub.java
@@ -0,0 +1,17 @@
+package com.sample.github;
+/**
+ * @author lammy-Sup
+ * @GitHub https://github.com/lammySup/
+ */
+public class HelloGitHub {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloGitHub");
+
+	}
+
+}
